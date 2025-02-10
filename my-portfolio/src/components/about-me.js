4cc255fdfcd15011e4828a47f8699a1abf381e41
Hxrshmxllow <86836424+Hxrshmxllow@users.noreply.github.com>
2025-02-10 10:20:37 -0500

    updated about-me

--- my-portfolio/src/components/about-me.js
+++ my-portfolio/src/components/about-me.js
@@ -4,12 +4,34 @@ import me from '../images/me.jpeg';
 import linkedin from '../images/linkedin.png';
 import email from '../images/email.png';
 import github from '../images/github.png';
+import { TypeAnimation } from 'react-type-animation';
+import { useState } from "react";
 
 function AboutMe() {
+  const [showCursor, setShowCursor] = useState(true);
   return (
     <section id="about">
       <div class='info_container'>
-        <h1 className="about_heading">Hello, I'm Harshit Patel</h1>
+       
+        <TypeAnimation
+          className="about-heading"
+          sequence={[
+            "Hello, I'm ",
+            1000,
+            "Hello, I'm Harshit Patel ",
+            () => setShowCursor(false),
+          ]}
+          wrapper="span"
+          speed={25}
+          cursor={showCursor}
+          style={{
+            fontSize: '2em',
+            display: 'inline-block',
+            backgroundImage: 'linear-gradient(to right, #91cae7, #5da6c3)',
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent'
+          }}
+        />
         <p>I am a student at Rutgers University studying Computer Science with a minor in Data Science and Quantitative Economics.</p>
         <div class="socials_container">
         <a href="https://www.linkedin.com/in/harshitp1206/" target="_blank" aria-label="LinkedIn">
